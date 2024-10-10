0154de3c64d9b09802801cef010b23073b936662
kwangjaeyoo <yookj00@gmail.com>
2024-10-10 16:42:04 +0900

    react native 75.4

--- App.tsx
+++ App.tsx
@@ -1,16 +1,10 @@
 import './global.css'
 import './gesture-handler'
 
-import {SafeAreaProvider} from 'react-native-safe-area-context'
-
 import StackNavigation from './src/navi/StackNavigation'
 
 function App(): React.JSX.Element {
-  return (
-    <SafeAreaProvider>
-      <StackNavigation />
-    </SafeAreaProvider>
-  )
+  return <StackNavigation />
 }
 
 export default App
